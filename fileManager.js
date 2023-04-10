e34763814a1aa28f44c3dc300e73e9ad5f5b08c1
Maxim Omelchuk <omelchukmaxim@gmail.com>
2023-04-11 00:21:48 +0300

    feat: add hello and goodbye messages

--- fileManager.js
+++ fileManager.js
@@ -1,1 +1,22 @@
-console.log("dere");
+const userName =
+  process.argv
+    .find((item) => item.startsWith("--username="))
+    ?.replace(/^--username=/, "") || "Unknown User";
+const helloMessage = `Welcome to the File Manager, ${userName}!\n`;
+const goodbyeMessage = `Thank you for using File Manager, ${userName}, goodbye!\n`;
+const goodbyeHandler = () => {
+  process.stdout.write(goodbyeMessage);
+  process.exit();
+};
+
+process.stdout.write(helloMessage);
+process.stdin.on("data", (data) => {
+  if (data.toString().slice(0, -2) === ".exit") {
+    goodbyeHandler();
+    return;
+  }
+  //   console.log("data");
+  //   process.exit();
+});
+
+process.on("SIGINT", goodbyeHandler);
